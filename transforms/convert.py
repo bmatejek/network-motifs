897dc3be51bcc709013d1f9a772696f9357b54a7
Brian Matejek <bmatejek@gmail.com>
2019-12-11 01:13:37 -0500

    transforms: convert traces to graph

--- transforms/convert.py
+++ transforms/convert.py
@@ -9,11 +9,19 @@ import pandas as pd
 
 
 from network_motifs.data_structures.open_stack import OpenStackTrace, OpenStackNode, OpenStackEdge
+from network_motifs.data_structures.trace import GetUniqueNames
 from network_motifs.data_structures.xtrace import XTrace, XTraceNode, XTraceEdge
+from network_motifs.utilities.dataIO import ReadFilenames, ReadTraces
+from network_motifs.utilities.constants import request_types_per_dataset
 
 
 
 def VerifyKeys(data, keys):
+    """
+    Verify that the data conatins all of the keys.
+    @params data: dictionary for verifcation
+    @params keys: keys to verify are in the dictionary
+    """
     # make sure every trace has these keys
     for key, value in data.items():
         assert (key in keys)
@@ -24,6 +32,11 @@ def VerifyKeys(data, keys):
 
 
 def ReadJSONTrace(dataset, json_filename):
+    """
+    Read the trace from the original JSON filename
+    @params dataset: the dataset name for the traces
+    @params json_filename: the JSON filename that contains the trace
+    """
     # generic read file for easier access to OpenStack and XTrace reads
     if dataset == 'openstack': return ReadOpenStackJSONTrace(json_filename)
     elif dataset == 'xtrace': return ReadXTraceJSONTrace(json_filename)
@@ -32,6 +45,11 @@ def ReadJSONTrace(dataset, json_filename):
 
 
 def ReadOpenStackJSONTrace(json_filename):
+    """
+    Read the OpenStack JSON trace into an OpenStackTrace data structure
+    and save to file.
+    @params json_filename: the JSON filename that contains the trace.
+    """
     # open the JSON file
     with open(json_filename, 'r') as fd:
         data = json.load(fd)
@@ -109,6 +127,11 @@ def ReadOpenStackJSONTrace(json_filename):
 
 
 def ReadXTraceJSONTrace(json_filename):
+    """
+    Read the XTrace JSON trace into an XTrace data structure
+    and save to file.
+    @params json_filename: the JSON filename that contains the trace.
+    """
     # open the JSON file
     with open(json_filename, 'r') as fd:
         data = json.load(fd)
@@ -195,6 +218,10 @@ def ReadXTraceJSONTrace(json_filename):
 
 
 def ConvertJSON2Trace(dataset):
+    """
+    Convert all of the JSON traces in the dataset into the .trace file type
+    @params dataset: the type of trace to convert
+    """
     # start statistics
     start_time = time.time()
 
@@ -204,3 +231,101 @@ def ConvertJSON2Trace(dataset):
         ReadJSONTrace(dataset, filename)
 
     print ('Converted JSON files for {} in {:0.2f} seconds.'.format(dataset, time.time() - start_time))
+
+
+
+def ConvertTrace2GramiGraph(dataset, request_type, traces):
+    """
+    Convert the trace into a grami graph where nodes stack on each other.
+    @params dataset: the dataset that the traces come from
+    @params request_type: the request type for this particular set of traces
+    @params traces: all of the traces for this dataset request type combo
+    """
+    # read the mapping from nodes to indices
+    _, name_to_index, _ = GetUniqueNames(dataset)
+
+    # create the grami file
+    grami_filename = 'graphs/{}/{}-grami.lg'.format(dataset, request_type)
+    with open(grami_filename, 'w') as fd:
+        # write the transaction id first
+        fd.write('# t 1\n')
+
+        # mapping from (trace, node) to index
+        node_mapping = {}
+        node_index = 0
+        # go through each trace and add the node to the file
+        for trace in traces:
+            # go through each node in this trace
+            for node in trace.nodes:
+                # check to make sure each node is unique
+                assert (not node in node_mapping)
+                node_mapping[node] = node_index
+                name_index = name_to_index[node.Name()]
+
+                fd.write('v {:07d} {:04d}\n'.format(node_index, name_index))
+
+                node_index += 1
+
+        # write all of the edges in the file
+        for trace in traces:
+            # go through each edge in the trace
+            for edge in trace.edges:
+                # get the source and destination for this node
+                source = edge.source
+                destination = edge.destination
+
+                source_index = node_mapping[source]
+                destination_index = node_mapping[destination]
+
+                fd.write('e {:07d} {:07d}\n'.format(source_index, destination_index))
+
+
+
+def ConvertTrace2GastonGraph(dataset, request_type, traces):
+    """
+    Convert the trace into a gaston graph where nodes stack on each other.
+    @params dataset: the dataset that the traces come from
+    @params request_type: the request type for this particular set of traces
+    @params traces: all of the traces for this dataset request type combo
+    """
+    # read the mapping from nodes to indices
+    _, name_to_index, _ = GetUniqueNames(dataset)
+
+    # create the gaston file
+    gaston_filename = 'graphs/{}/{}-gaston.lg'.format(dataset, request_type)
+    with open(gaston_filename, 'w') as fd:
+        # go through each trace and add the node and edges to the file
+        for trace_index, trace in enumerate(traces):
+            fd.write('t # {}\n'.format(trace_index))
+
+            # go through each node in this trace
+            for node in trace.nodes:
+                # check to make sure each node is unique
+                name_index = name_to_index[node.Name()]
+
+                fd.write('v {:07d} {:04d}\n'.format(node.index, name_index))
+
+            # go through each edge in the trace
+            for edge in trace.edges:
+                # get the source and destination for this node
+                source_index = edge.source.index
+                destination_index = edge.destination.index
+
+                fd.write('e {:07d} {:07d}\n'.format(source_index, destination_index))
+
+
+
+def ConvertTrace2Graph(dataset):
+    """
+    Convert the traces for this dataset into formats for both GraMi and Gaston
+    @params dataset: the type of trace to convert into motif discovery formats
+    """
+    # get all of the request types for this dataset
+    for request_type in request_types_per_dataset[dataset]:
+        print ('{} {}'.format(dataset, request_type))
+        # read all the traces from this dataset
+        trace_filenames = ReadFilenames(dataset, request_type)
+        traces = ReadTraces(dataset, trace_filenames)
+
+        ConvertTrace2GramiGraph(dataset, request_type, traces)
+        ConvertTrace2GastonGraph(dataset, request_type, traces)
