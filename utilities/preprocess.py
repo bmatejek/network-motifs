e63169cb7efb1c9ac9e2c91a959af504d3045b69
Brian Matejek <bmatejek@gmail.com>
2019-12-11 20:41:52 -0500

    preprocess: move pruned traces to a new directory for backup

--- utilities/preprocess.py
+++ utilities/preprocess.py
@@ -14,6 +14,10 @@ def PruneXTraceJSONFiles(request_types):
     # start statistics
     start_time = time.time()
 
+    # make pruned directory if it does not exist
+    if not os.path.exists('jsons/xtrace/pruned-traces'):
+        os.mkdir('jsons/xtrace/pruned-traces')
+
     # expect the xtrace files to be in this directory
     xtrace_filenames = sorted(glob.glob('jsons/xtrace/*.json'))
 
@@ -27,6 +31,9 @@ def PruneXTraceJSONFiles(request_types):
         # read this xtrace json file
         with open(xtrace_filename, 'r') as fd:
             data = json.load(fd)
+
+        # get the base id for this file
+        base_id = data['id']
 
         # consider all of the nodes in the list of reports
         reports = data['reports']
@@ -45,7 +52,8 @@ def PruneXTraceJSONFiles(request_types):
         # if we should remove the trace, make sure there are no tags
         if remove_trace:
             assert (not len(tags))
-            os.remove(xtrace_filename)
+            pruned_filename = 'jsons/xtrace/pruned-traces/{}.json'.format(base_id)
+            os.rename(xtrace_filename, pruned_filename)
         # otherwise make sure there is only one request
         else:
             # make sure that there is only one tag per requeset
