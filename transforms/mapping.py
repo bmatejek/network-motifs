6cf31162ab342c88a9f1fdcdaf9564888237817f
Brian Matejek <bmatejek@gmail.com>
2019-12-11 21:01:51 -0500

    transforms: updated mapping with statistics and documentation

--- transforms/mapping.py
+++ transforms/mapping.py
@@ -1,8 +1,26 @@
+import os
+import time
+
+
 from network_motifs.utilities import dataIO
 
 
 
 def CreateNameMapping(dataset):
+    """
+    Create a mapping between all of the names in the dataset and a unique ID.
+    The mapping is called from the function GetUniqueNames in trace.py.
+    @params dataset: the dataset for which to create the mapping
+    """
+    # start statistics
+    start_time = time.time()
+
+    # create the directories for the mappings
+    if not os.path.exists('mappings'):
+        os.mkdir('mappings')
+    if not os.path.exists('mappings/{}'.format(dataset)):
+        os.mkdir('mappings/{}'.format(dataset))
+
     filenames = dataIO.ReadFilenames(dataset)
 
     # create a set of all possible function names
@@ -16,7 +34,11 @@ def CreateNameMapping(dataset):
 
     names = sorted(list(names))
 
+    # output the mapping to disk
     output_filename = 'mappings/{}/name-to-index.txt'.format(dataset)
     with open(output_filename, 'w') as fd:
         for name in names:
             fd.write('{}\n'.format(name))
+
+    # print statistics
+    print ('Created function name mappings for {} in {:0.2f} seconds.'.format(dataset, time.time() - start_time))
